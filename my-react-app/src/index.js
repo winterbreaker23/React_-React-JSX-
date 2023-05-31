57aea5a018eb63446a3e441843c9925eb85345a9
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 14:43:34 +0100

    JSX-exp4

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -14,8 +14,13 @@ const myelement3=(
     <li>Basketball</li>
   </ul>
 )
-
-root.render(myelement3);
+const myelement4 =(
+  <div>
+    <p>I am an engineer</p>
+    <p>I am an engineer too</p>
+  </div>
+);
+root.render(myelement4);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
