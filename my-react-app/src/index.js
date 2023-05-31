fc7861d36f883e3e320bb035554c2e36c0dfec34
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 14:39:30 +0100

    JSX-exp2

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const myelement = <h1>hello!</h1>;
-root.render(myelement);
+const myelement2=<h1>React is {50+50} times better with JSX</h1>;
+root.render(myelement2);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
