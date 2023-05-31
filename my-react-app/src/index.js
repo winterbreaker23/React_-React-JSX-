fef1b351f90b5ba010aee71b35a373d83ceac36b
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 14:43:11 +0100

    JSX-exp3

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -7,7 +7,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const myelement = <h1>hello!</h1>;
 const myelement2=<h1>React is {50+50} times better with JSX</h1>;
-root.render(myelement2);
+const myelement3=(
+  <ul>
+    <li>Football</li>
+    <li>Volleyball</li>
+    <li>Basketball</li>
+  </ul>
+)
+
+root.render(myelement3);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
